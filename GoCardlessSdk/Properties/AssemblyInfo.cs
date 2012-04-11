2f474481dfae8bd73fc5220e8a1804fd9fb178f7
tim_iles <tim@timiles.com>
2012-04-11 19:15:14 +0100

    description

--- GoCardlessSdk/Properties/AssemblyInfo.cs
+++ GoCardlessSdk/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GoCardless")]
-[assembly: AssemblyDescription("API SDK for GoCardless.com")]
+[assembly: AssemblyDescription("GoCardless API SDK")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GoCardless")]
 [assembly: AssemblyProduct("GoCardless")]
